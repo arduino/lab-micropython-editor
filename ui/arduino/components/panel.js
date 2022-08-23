e8a9e9d7a82880e5e1af2df795512166a160cb26
Murilo Polese <murilopolese@gmail.com>
2022-08-23 16:43:08 +0200

    Editor size fix

--- ui/arduino/components/panel.js
+++ ui/arduino/components/panel.js
@@ -1,6 +1,7 @@
 function Panel(state, emit) {
+  let openClass = state.isTerminalOpen || state.isFilesOpen ? 'open' : ''
   return html`
-    <div id="panel">
+    <div id="panel" class=${openClass}>
       ${state.isTerminalOpen ? PanelTerminal(state, emit) : null}
       ${state.isFilesOpen ? PanelFiles(state, emit) : null}
     </div>

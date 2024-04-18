8515dd52bc9c28d2a1bb5a78c41a1d39be81cd5a
Murilo Polese <murilopolese@gmail.com>
2024-04-18 12:32:35 +0200

    Remember scroll position between tabs

--- ui/arduino/views/components/elements/editor.js
+++ ui/arduino/views/components/elements/editor.js
@@ -3,15 +3,31 @@ class CodeMirrorEditor extends Component {
     super()
     this.editor = null
     this.content = '# empty file'
+    this.scrollTop = 0
   }
 
   load(el) {
     const onCodeChange = (update) => {
-      // console.log('code change', this.content)
       this.content = update.state.doc.toString()
       this.onChange()
     }
     this.editor = createEditor(this.content, el, onCodeChange)
+    this.editor.scrollDOM.addEventListener('scroll', this.updateScrollPosition.bind(this))
+    setTimeout(() => {
+      this.editor.scrollDOM.scrollTo({
+        top: this.scrollTop,
+        left: 0
+      })
+    }, 1)
+  }
+
+  updateScrollPosition(e) {
+    console.log(e.target.scrollTop)
+    this.scrollTop = e.target.scrollTop
+  }
+
+  unload() {
+    this.editor.scrollDOM.removeEventListener('scroll', this.updateScrollPosition)
   }
 
   createElement(content) {

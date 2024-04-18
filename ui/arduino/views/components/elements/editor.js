fef591641750221b9578710b2fdc16e201040551
Murilo Polese <murilopolese@gmail.com>
2024-04-18 15:09:08 +0200

    Increase timeout

--- ui/arduino/views/components/elements/editor.js
+++ ui/arduino/views/components/elements/editor.js
@@ -6,37 +6,39 @@ class CodeMirrorEditor extends Component {
     this.scrollTop = 0
   }
 
+  createElement(content) {
+    if (content) this.content = content
+    return html`<div id="code-editor"></div>`
+  }
+
+
   load(el) {
     const onCodeChange = (update) => {
       this.content = update.state.doc.toString()
       this.onChange()
     }
     this.editor = createEditor(this.content, el, onCodeChange)
-    this.editor.scrollDOM.addEventListener('scroll', this.updateScrollPosition.bind(this))
+
     setTimeout(() => {
+      this.editor.scrollDOM.addEventListener('scroll', this.updateScrollPosition.bind(this))
       this.editor.scrollDOM.scrollTo({
         top: this.scrollTop,
         left: 0
       })
-    }, 1)
+    }, 10)
   }
 
-  updateScrollPosition(e) {
-    console.log(e.target.scrollTop)
-    this.scrollTop = e.target.scrollTop
+  update() {
+    return false
   }
 
   unload() {
     this.editor.scrollDOM.removeEventListener('scroll', this.updateScrollPosition)
   }
 
-  createElement(content) {
-    if (content) this.content = content
-    return html`<div id="code-editor"></div>`
-  }
-
-  update() {
-    return false
+  updateScrollPosition(e) {
+    console.log(e.target.scrollTop)
+    this.scrollTop = e.target.scrollTop
   }
 
   onChange() {

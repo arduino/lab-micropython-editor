689863a6578603e500a1dc27502d98513beb6196
ubi de feo <me@ubidefeo.com>
2024-12-21 09:53:56 +0100

    Fixed file actions buttons states and CSS.
    Signed-off-by: ubi de feo <me@ubidefeo.com>

--- ui/arduino/views/components/elements/button.js
+++ ui/arduino/views/components/elements/button.js
@@ -1,33 +1,34 @@
 function Button(args) {
   const {
+    first = false,
     size = '',
+    square = false,
     icon = 'connect.svg',
-    onClick = (e) => false,
+    onClick = (e) => {console.log(e); false},
     disabled = false,
     active = false,
-    label,
     tooltip,
-    background,
-    first,
-    square
+    label,
+    background
   } = args
   let tooltipEl = html``
   if (tooltip) {
     tooltipEl = html`<div class="tooltip">${tooltip}</div>`
   }
-  let activeClass = active ? 'active' : 'inactive'
+  let activeClass = active ? 'active' : ''
   let backgroundClass = background ? 'inverted' : ''
-  let labelActiveClass = disabled ? 'inactive' : 'active'
   let buttonFirstClass = first ? 'first' : ''
   let squareClass = square ? 'square' : ''
+  let labelActiveClass = disabled ? 'inactive' : 'active'
   let labelItem = size === 'small' ? '' : html`<div class="label ${labelActiveClass}">${label}</div>`
+
   return html`
-    <div class="button ${buttonFirstClass}">
-      <button disabled=${disabled} class="${squareClass}${size} ${activeClass} ${backgroundClass}" onclick=${onClick}>
-        <img class="icon" src="media/${icon}" />
-      </button>
-      ${labelItem}
-      ${tooltipEl}
-    </div>
-  `
+     <div class="button ${buttonFirstClass}">
+       <button disabled=${disabled} class="${squareClass}${size} ${activeClass} ${backgroundClass}" onclick=${onClick}>
+         <img class="icon" src="media/${icon}" />
+       </button>
+       ${labelItem}
+       ${tooltipEl}
+     </div>
+   `
 }

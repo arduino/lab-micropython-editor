3b39480774fe6060450a2d79ba778d413dbdbc7c
ubi de feo <me@ubidefeo.com>
2024-12-27 10:48:30 +0100

    Testing Hack font, since RobotoMono failed to display.
    Signed-off-by: ubi de feo <me@ubidefeo.com>

--- ui/arduino/main.js
+++ ui/arduino/main.js
@@ -41,7 +41,28 @@ function App(state, emit) {
   `
 }
 
+// document.fonts.ready.then(() => {
+//   console.log("Fonts loaded");
+//   console.log(document.fonts.check('12px RobotoMono'));
+// });
+
 window.addEventListener('load', () => {
+  // const fontRobotoMono = new FontFace('RobotoMono', 'url(media/roboto-mono-latin-ext-400-normal.woff2)', {
+  //   style: 'normal',
+  //   weight: '400',
+  //   display: 'swap'
+  // });
+  // fontRobotoMono.load().then((loadedFace) => {
+  //   document.fonts.add(loadedFace);
+  //   // The font will now be available for your CSS to use
+  //   console.log('RobotoMono properties:', loadedFace);
+  // });
+  // const fontHack = new FontFace('Hack', 'url(media/hack-regular.woff2)');
+  // fontHack.load().then((loadedFace) => {
+  //   document.fonts.add(loadedFace);
+  //   // The font will now be available for your CSS to use
+  //   console.log('loaded face: ', loadedFace);
+  // });
   let app = Choo()
   app.use(store);
   app.route('*', App)

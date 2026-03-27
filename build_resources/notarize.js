aec6e737b380ebcf3d0f5ac7cbcd9031c203303b
ubi de feo <me@ubidefeo.com>
2026-03-27 10:54:10 +0100

    Updated Arduino company description.
    Signed-off-by: ubi de feo <me@ubidefeo.com>

--- build_resources/notarize.js
+++ build_resources/notarize.js
@@ -1,4 +1,4 @@
-// Copyright 2022 Arduino SA
+// Copyright 2022 Arduino s.r.l. and/or its affiliated companies
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
